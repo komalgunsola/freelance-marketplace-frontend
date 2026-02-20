89f6916a836142d76e5991d6135ab9beb829f7e5
Komal Gunsola <komalgunsola28@gmail.com>
2026-02-20 18:07:21 +0530

    backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://freelance-marketplace-backend-dh2jsa3bd.vercel.app/api"
 });
 
 export default API;
